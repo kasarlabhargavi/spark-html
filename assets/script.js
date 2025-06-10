6dc2d3bb6cd1678c16dc9a68a124ab6c1a03a991
kasarlabhargavi <kasarlabhargavi03@gmail.com>
2025-06-10 15:40:50 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+
+  $(document).ready(function () {
+    $('.brand-slider').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      dots: false,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    });
+  });
